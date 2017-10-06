09e31e9b73438401ed2a0e20c19a532b02be2989
Ciarán Ó hAoláin <c.ohaolain@gmail.com>
2017-10-06 10:35:17 +0100

    Add BigIntFactorial and LuhnAlgorithm
    Change-Id: I574cb4011e99580731a0c348b518079bd1affeb3

---
+++ BigIntFactorial/src/BigIntFactorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class BigIntFactorial{
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		long num = input.nextLong();
+		String answer = returnFactorial(num);
+		System.out.println(num + "! = " + answer);
+		input.close();
+	}
+	
+	public static String returnFactorial(long n) {
+		BigInteger nBig = new BigInteger(Long.toString(n));
+		for (long i=Math.abs(n); i>1; i--) {
+			BigInteger iBig = new BigInteger(Long.toString(i-1));
+			nBig = nBig.multiply(iBig);
+		}
+		return nBig.toString();
+	}
+	
+}
